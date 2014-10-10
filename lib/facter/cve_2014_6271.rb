6c8d3b81a9d0bea812cfae83d7f8493f5422eff8
Garrett Honeycutt <code@garretthoneycutt.com>
2014-10-10 17:28:54 -0400

    Add CVE link and shellshock monkiker to cve_2014_6271

--- lib/facter/cve_2014_6271.rb
+++ lib/facter/cve_2014_6271.rb
@@ -1,3 +1,6 @@
+# https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2014-6271
+# ShellShock
+#
 # From https://github.com/renanvicente/puppet-shellshock
 Facter.add('cve_2014_6271') do
   setcode do
